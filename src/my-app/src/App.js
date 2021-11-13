ed74e590184ed920063e39c874a03d0cf90795dd
rajoriaakash <akashrajoria250103@gmail.com>
2021-11-13 22:50:32 +0530

    Link categories to their respective routes

--- src/my-app/src/App.js
+++ src/my-app/src/App.js
@@ -4,7 +4,8 @@ import Footer from './components/Footer.js'
 import UpperSlide from './components/UpperSlides.js'
 import AboutUs from './components/AboutUs.js'
 import Youtube from './components/Youtube.js'
-import Products from './components/Products.js'
+import Products from './components/Products'
+
 function App () {
   return (
     <div id="home-section">
--- src/my-app/src/components/Products.js
+++ src/my-app/src/components/Products.js
@@ -4,14 +4,24 @@ import HOMEDECOR from '../images/HOMEDECOR.jpg'
 import FURNISHING from '../images/FURNISHING.jpg'
 import ACCESSORIES from '../images/ACCESSORIES.jpg'
 import CLOTHES from '../images/CLOTHES.jpg'
+import { Link } from 'react-router-dom'
 function Products () {
+  const category = ["HOMEDECOR","FURNISHING","ACCESSORIES","CLOTHES"];
   return (
     <div className="products" id="products-section">
       <h1 style={{marginLeft:'4.5%',paddingBottom:'3%'}}>Our Products</h1>
-      <Card img={HOMEDECOR}  product="HOMEDECOR"/>
-      <Card img={FURNISHING}  product="FURNISHING"/>
-      <Card img={ACCESSORIES}  product="ACCESSORIES"/>
-      <Card img={CLOTHES} product="CLOTHES" />
+      <Link to= {`products/${category[0]}`}>
+      <Card img={HOMEDECOR}  product={category[0]}/>
+      </Link>
+      <Link to= {`products/${category[1]}`}>
+      <Card img={FURNISHING}  product={category[1]}/>
+      </Link>
+      <Link to= {`products/${category[2]}`}>
+      <Card img={ACCESSORIES}  product={category[2]}/>
+      </Link>
+      <Link to= {`products/${category[3]}`}>
+      <Card img={CLOTHES} product={category[3]} />
+      </Link>
     </div>
   )
 }
